f408d1bc2ba2b436e34b20e607c02f9ba74f91b3
Steve Ramage <sramage@nucleuslabs.com>
2017-05-04 14:40:39 -0700

    Added 2 rules digits and realNumber

--- src/Rules.js
+++ src/Rules.js
@@ -33,12 +33,12 @@ const required = custom(isFilled, {
     isOptional: false,
 });
 
-function minLength(length,message=(value,length) => `Please enter at least ${length} characters (${length - value.length} more).`) {
-    return custom(val => val.length >= length, {id:'minLength',message: value => message(value,length)});
+function minLength(length, message = (value, length) => `Please enter at least ${length} characters (${length - value.length} more).`) {
+    return custom(val => val.length >= length, {id: 'minLength', message: value => message(value, length)});
 }
 
-function maxLength(length,message=(value,length) => `Please enter at most ${length} characters. You've entered ${value.length}.`) {
-    return custom(val => val.length <= length, {id:'maxLength',message: value => message(value,length)});
+function maxLength(length, message = (value, length) => `Please enter at most ${length} characters. You've entered ${value.length}.`) {
+    return custom(val => val.length <= length, {id: 'maxLength', message: value => message(value, length)});
 }
 
 const email = custom(value => /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i.test(value), {
@@ -46,18 +46,31 @@ const email = custom(value => /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)
     message: `Please enter a valid email address.`,
 });
 
-function min(min, message=(value, min) => `Please enter a value greater than or equal to ${min}.`) {
-    return custom(value => !isNaN(value) && value >= min, {id:'min',message: value => message(value, min)});
+function min(min, message = (value, min) => `Please enter a value greater than or equal to ${min}.`) {
+    return custom(value => !isNaN(value) && value >= min, {id: 'min', message: value => message(value, min)});
 }
 
-function max(max, message=(value, max) => `Please enter a value less than or equal to ${max}.`) {
-    return custom(value => !isNaN(value) && value <= max, {id:'max',message: value => message(value, max)});
+function max(max, message = (value, max) => `Please enter a value less than or equal to ${max}.`) {
+    return custom(value => !isNaN(value) && value <= max, {id: 'max', message: value => message(value, max)});
 }
 
-function range(min, max, message=(value, min, max) => `Please enter a value between ${min} and ${max} inclusive.`) {
-    return custom(value => !isNaN(value) && value >= min && value <= max, {id:'range',message: value => message(value, min, max)});
+function range(min, max, message = (value, min, max) => `Please enter a value between ${min} and ${max} inclusive.`) {
+    return custom(value => !isNaN(value) && value >= min && value <= max, {id: 'range', message: value => message(value, min, max)});
+}
+
+function digits(message = (value) => `Please enter only digits.`) {
+    return custom(value => /^\d*$/.test(value), message());
+}
+
+/**
+ * @param {bool} negative
+ * @param message
+ * @returns {{id, isAsync, message, validate, precondition, dependsOn, isOptional, type, compare}|*}
+ */
+function realNumber(negative = false, message = (value) => `Please enter a valid number.`) {
+    return custom(value => (!negative || value >= 0) && /^-?(?:\d+|\d{1,3}(?:,\d{3})+)?(?:\.\d+)?$/.test(value), message());
 }
 
 // TODO: add rest from https://jqueryvalidation.org/documentation/#link-list-of-built-in-validation-methods
 
-module.exports = {required, minLength, maxLength, email, custom, async, min, max, range};
+module.exports = {required, minLength, maxLength, email, custom, async, min, max, range, digits, realNumber};
