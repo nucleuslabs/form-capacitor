eef3f9894d5943f7e5db6d27154862b6128033d8
Mark Penner <mpenner@nucleuslabs.com>
2018-05-17 19:01:06 -0700

    Did checkboxes and textboxes

--- packages/examples3/src/components/controls/Checkbox.js
+++ packages/examples3/src/components/controls/Checkbox.js
@@ -1,27 +1,17 @@
 import {Checkbox as Component} from '../bulma';
 import connect from '../../form-capacitor/connect';
 import {action} from 'mobx';
-import {mount} from '../../form-capacitor';
+import {consumeValue, mount} from '../../form-capacitor';
 
-@mount({
-    defaultValue: false,
-    path: p => p.name,
-})
-@connect({
-    propName: 'checked',
-})
-export default class Checkbox extends React.Component {
 
-    @action.bound
-    handleChange(ev) {
-        this.checked = ev.target.checked;
-        if(this.props.onChange) {
-            this.props.onChange(ev)
-        }
+@consumeValue({name:'checked'})
+export default class TextArea extends React.Component {
+    handleChange = ev => {
+        this.props.setValue(ev.target.checked);
     }
 
     render() {
-        const {checked, value, defaultValue, name, ...props} = this.props;
-        return <Component {...props} checked={this.checked} onChange={this.handleChange}/>
+        const { setValue, ...props} = this.props;
+        return <Component {...props} onChange={this.handleChange}/>
     }
 }
--- packages/examples3/src/components/controls/TextBox.js
+++ packages/examples3/src/components/controls/TextBox.js
@@ -1,27 +1,15 @@
-import {InputText} from '../bulma';
-import connect from '../../form-capacitor/connect';
-import {action} from 'mobx';
-import {mount} from '../../form-capacitor';
+import {InputText as Component} from '../bulma';
+import {consumeValue} from '../../form-capacitor';
 
-@mount({
-    defaultValue: p => p.defaultValue !== undefined ? p.defaultValue : '',
-    path: p => p.name,
-})
-@connect({
-    propName: 'value',
-})
-export default class TextBox extends React.Component {
 
-    @action.bound
-    handleChange(ev) {
-        this.value = ev.target.value;
-        if(this.props.onChange) {
-            this.props.onChange(ev)
-        }
+@consumeValue()
+export default class TextArea extends React.Component {
+    handleChange = ev => {
+        this.props.setValue(ev.target.value);
     }
-    
+
     render() {
-        const {value, name, defaultValue, ...props} = this.props;
-        return <InputText {...props} value={this.value} onChange={this.handleChange}/>
+        const {setValue, ...props} = this.props;
+        return <Component {...props} onChange={this.handleChange}/>
     }
 }
