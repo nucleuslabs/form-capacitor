206dfb19afcb1259e2b7db4bccbc0a3670c94b46
Steve Ramage <sramage@nucleuslabs.com>
2019-09-03 09:07:17 -0700

    Exported hooks methods so that everyone can use them... whoops

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 export {default as schema} from './schema';
 export {default as consumeValue, consumeArrayValue} from './consume';
 export {setValue,getValue,errorMapToFlatArray} from './helpers';
+export {default as useSchema} from './useSchema';
+export {default as useConsume} from './useConsume';
+export {default as useConsumeArray} from './useConsumeArray';
+export {default as useConsumeErrors} from './useConsumeErrors';
