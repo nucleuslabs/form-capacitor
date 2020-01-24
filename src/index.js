44fb5dc3c36e0c3c95b92a4cd60fd5206b13b702
Steve Ramage <sramage@nucleuslabs.com>
2020-01-24 14:11:17 -0800

    Removed useSchemaPath function it was jank and has been replaced by <SubSchema/>

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ export {setValue, getValue} from './helpers';
 export {getFlattenedErrors, getErrors, getErrorNode, setError, setErrors} from './errorMapping';
 export {default as useSchema} from './useSchema';
 export {default as SubSchema} from './SubSchema';
-export {default as useSchemaPath} from './useSchemaPath';
 export {default as useConsume} from './useConsume';
 export {default as useConsumeArray} from './useConsumeArray';
 export {default as useConsumeErrors} from './useConsumeErrors';
